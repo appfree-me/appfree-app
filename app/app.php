2d0aff309e06c6c83cb14047f7c105b815de530a
Your Name <you@example.com>
2024-09-26 17:30:54 +0200

    build, deploy working

---
+++ app/app.php
@@ -0,0 +1,7 @@
+<?php
+
+
+echo "Starting ARI Connection\n";
+$ariConnector = new phpari();
+echo "Active Channels: " . json_encode($ariConnector->channels()->channel_list()) . "\n";
+echo "Ending ARI Connection\n";
