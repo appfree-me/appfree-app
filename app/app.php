0ea30018f32002c90b7d14dd61e2bb867fea96c2
Your Name <you@example.com>
2024-10-15 20:11:56 +0200

    pjsip seemingly working

--- app/app.php
+++ app/app.php
@@ -1,7 +1,15 @@
 <?php
 
+//namespace app;
 
-echo "Starting ARI Connection\n";
-$ariConnector = new phpari();
-echo "Active Channels: " . json_encode($ariConnector->channels()->channel_list()) . "\n";
-echo "Ending ARI Connection\n";
+use phpari;
+
+//echo "Starting ARI Connection\n";
+//$ariConnector = new phpari();
+//echo "Active Channels: " . json_encode($ariConnector->channels()->channel_list()) . "\n";
+//echo "Ending ARI Connection\n";
+
+require("BasicStasisApplication.php");
+$app = new BasicStasisApplication("appfree");
+
+$app->execute();
