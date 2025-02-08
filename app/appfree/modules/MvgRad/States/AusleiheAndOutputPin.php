ca1f9a55cff1751b37f1e91b2be7b2eed09b5818
Laurent <laurent.pi@gmail.com>
2025-02-08 10:13:00 +0100

    MVG Repeat PIN Output

--- app/appfree/modules/MvgRad/States/AusleiheAndOutputPin.php
+++ app/appfree/modules/MvgRad/States/AusleiheAndOutputPin.php
@@ -3,13 +3,8 @@ declare(strict_types=1);
 
 namespace AppFree\appfree\modules\MvgRad\States;
 
-use AppFree\AppController;
-use AppFree\appfree\modules\MvgRad\Api\MvgRadModule;
 use AppFree\AppFreeCommands\AppFree\Expectations\PlaybackFinishedExpectation;
 use AppFree\AppFreeCommands\MvgRad\Commands\V1\MvgRadAusleiheCommand;
-use AppFree\AppFreeCommands\Stasis\Events\V1\PlaybackFinished;
-use AppFree\Ari\PhpAri;
-use Swagger\Client\Api\ChannelsApi;
 
 
 class AusleiheAndOutputPin extends MvgRadState
@@ -19,16 +14,16 @@ class AusleiheAndOutputPin extends MvgRadState
     public function run(): \Generator
     {
         $ctx = $this->sm->getContext();
-
         $dto = yield "expect" => MvgRadAusleiheCommand::class;
-
         $pin = $this->mvgRadApi->doAusleihe($dto->radnummer);
-        $wait = $ctx->play(self::SOUND_PIN_IS);
-        yield "expect" => new PlaybackFinishedExpectation($wait);
 
-        $lastPlaybackId = $ctx->sayDigits($pin);
+        for ($i = 0; $i < 10; $i++) {
+            $wait = $ctx->play(self::SOUND_PIN_IS);
+            yield "expect" => new PlaybackFinishedExpectation($wait);
 
-        yield "expect" => new PlaybackFinishedExpectation($lastPlaybackId);
+            $lastPlaybackId = $ctx->sayDigits($pin);
+            yield "expect" => new PlaybackFinishedExpectation($lastPlaybackId);
+        }
 
         $ctx->hangup();
     }
