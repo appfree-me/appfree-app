5dd69daae2294b0b13ac179db3a8726b8a57f2e8
Laurent <laurent.pi@gmail.com>
2025-03-04 15:16:59 +0100

    require php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude(['var', 'node_modules'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'yoda_style' => false,
+        'single_line_throw' => false,
+    ])
+    ->setFinder($finder)
+;
