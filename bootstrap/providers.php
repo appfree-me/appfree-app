23b746e1faa8c7f1be8b6c755403b3edb5b008a4
Laurent Pichler <laurent.pichler@gmail.com>
2025-03-31 13:43:37 +0200

    chore: reorder

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,12 +2,12 @@
 
 return [
     AppFree\Providers\AppControllerServiceProvider::class,
-    AppFree\Providers\PhpAriServiceProvider::class,
-    AppFree\Providers\PhpAriConfigServiceProvider::class,
+    AppFree\Providers\EventEmitterServiceProvider::class,
     AppFree\Providers\GuzzleHttpServiceProvider::class,
     AppFree\Providers\LoggerServiceProvider::class,
     AppFree\Providers\MvgRadStateMachineServiceProvider::class,
+    AppFree\Providers\PhpAriServiceProvider::class,
+    AppFree\Providers\PhpAriConfigServiceProvider::class,
     AppFree\Providers\PromiseInterfaceServiceProvider::class,
-    AppFree\Providers\EventEmitterServiceProvider::class,
     AppFree\Providers\WatchdogControllerServiceProvider::class,
 ];
